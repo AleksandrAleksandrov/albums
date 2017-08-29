345e86a723c35084e1a856c101ac621e885ffea7
AleksandrAleksandrov <aleksandrggggg@gmail.com>
2017-08-29 16:18:29 +0300

    albums test application finished

---
+++ index.android.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// Create Component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+
+);
+
+// Render
+AppRegistry.registerComponent('albums', () => App);
